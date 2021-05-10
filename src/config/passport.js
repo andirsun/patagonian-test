5afda17a7c42ce8c7bb25d69a90ac425874ad1f4
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-05-09 22:37:52 -0500

    feat: server configurations

---
+++ src/config/passport.js
@@ -0,0 +1,23 @@
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const config = require('./config');
+// const { Usuario } = require('../models');
+
+const jwtOptions = {
+  secretOrKey: config.jwt.secret,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
+
+const jwtVerify = async (payload, done) => {
+  try {
+
+    done(null, {});
+  } catch (error) {
+    done(error, false);
+  }
+};
+
+const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
+
+module.exports = {
+  jwtStrategy,
+};
