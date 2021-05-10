4ab1b09446121fed9b5a7e49d1f55b0af3332c64
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-05-09 22:52:09 -0500

    feat: sequelize migrations and model

---
+++ src/migrations/20210510034929-create-car.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cars', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      UUID: {
+        type: Sequelize.STRING,
+      },
+      VIN: {
+        type: Sequelize.STRING,
+      },
+      make: {
+        type: Sequelize.STRING,
+      },
+      model: {
+        type: Sequelize.STRING,
+      },
+      mileage: {
+        type: Sequelize.INTEGER,
+      },
+      year: {
+        type: Sequelize.INTEGER,
+      },
+      price: {
+        type: Sequelize.STRING,
+      },
+      zip_code: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Cars');
+  },
+};
---
+++ src/models/car.js
@@ -0,0 +1,29 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Car extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Car.init(
+    {
+      UUID: DataTypes.STRING,
+      VIN: DataTypes.STRING,
+      make: DataTypes.STRING,
+      model: DataTypes.STRING,
+      mileage: DataTypes.INTEGER,
+      year: DataTypes.INTEGER,
+      price: DataTypes.STRING,
+      zip_code: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Car',
+      tableName: 'car',
+      updatedAt: 'updated_at',
+      createdAt: 'created_at',
+    }
+  );
+  return Car;
+};
